3f7a12c72ca620413cfa6d0e384c4700b40ef37b
connor-k <quikcon@cox.net>
2014-01-13 17:10:41 -0800

    Fixed sliding life seekbar, fixed gameover dialog, added more comments

--- src/com/connork/mtgcounter/MainActivity.java
+++ src/com/connork/mtgcounter/MainActivity.java
@@ -34,8 +34,9 @@ public class MainActivity extends Activity implements OnClickListener {
 	public static String KEY_PLAYER_NAMES = "mtg_player_names";
 	public static String KEY_STARTING_LIFE = "mtg_starting_life";
 
-	private int NUM_PLAYERS, STARTING_LIFE, selected_p13 = 1, selected_p24 = 2;
+	private int NUM_PLAYERS, STARTING_LIFE, selected_p13 = 0, selected_p24 = 1, PLAYER_1 = 0, PLAYER_2 = 1, PLAYER_3 = 2, PLAYER_4 = 3;
 	private int[] life;
+	private boolean[] alive = {false, false, false, false};
 	private String[] PLAYER_NAMES;
 	private TextView[] TEXTVIEW_LIFE;
 	private SeekBar[] PROGRESSBAR_LIFE;
@@ -47,7 +48,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		// Get passed information
+		// Get passed information and initialize player names, life, etc
 		NUM_PLAYERS = getIntent().getIntExtra(KEY_NUM_PLAYERS, 2);
 		String[] names = getIntent().getStringArrayExtra(KEY_PLAYER_NAMES);
 		STARTING_LIFE = getIntent().getIntExtra(KEY_STARTING_LIFE, 20);
@@ -57,27 +58,37 @@ public class MainActivity extends Activity implements OnClickListener {
 		for (int i = 0; i < NUM_PLAYERS; i++) {
 			PLAYER_NAMES[i] = names[i];
 			life[i] = STARTING_LIFE;
+			alive[i] = true;
 		}
 
-		// Restore last life state if possible
+		//TODO Restore last life state if possible
 		if (savedInstanceState != null) {
 			life = savedInstanceState.getIntArray("main_life");
 		}
 
+		// The textview and progressbars to keep track of player life
 		TEXTVIEW_LIFE = new TextView[NUM_PLAYERS];
 		PROGRESSBAR_LIFE = new SeekBar[NUM_PLAYERS];
 
+		// Set up all the views and change the layout based on the number of players
+		// The switch has no breaks, so each case sets up that player number (with case 2 setting up players 1 and 2)
+		//TODO make landscape layouts, tablet ui?
 		switch (NUM_PLAYERS) {
-		case 4: 
+		case 4:
+			// Use the 4-player layout
 			setContentView(R.layout.activity_main4);
-			((TextView)findViewById(R.id.textview_main_p4_name)).setText(PLAYER_NAMES[3]);
+			
+			// Set Player 4's name
+			((TextView)findViewById(R.id.textview_main_p4_name)).setText(PLAYER_NAMES[PLAYER_4]);
 
-			TEXTVIEW_LIFE[3] = (TextView)findViewById(R.id.textview_main_p4_life);
-			PROGRESSBAR_LIFE[3] = (SeekBar) findViewById(R.id.progressbar_main_p4_life);
-			radio_button[1] = (RadioButton) findViewById(R.id.radio_main_p2);
-			radio_button[1].setText(PLAYER_NAMES[1]);
-			radio_button[3] = (RadioButton) findViewById(R.id.radio_main_p4);
-			radio_button[3].setText(PLAYER_NAMES[3]);
+			// Player 4's life text and progressbar
+			TEXTVIEW_LIFE[PLAYER_4] = (TextView)findViewById(R.id.textview_main_p4_life);
+			PROGRESSBAR_LIFE[PLAYER_4] = (SeekBar) findViewById(R.id.progressbar_main_p4_life);
+			// The radio group to select who the +-life buttons route to (player 2 or 4) 
+			radio_button[PLAYER_2] = (RadioButton) findViewById(R.id.radio_main_p2);
+			radio_button[PLAYER_2].setText(PLAYER_NAMES[1]);
+			radio_button[PLAYER_4] = (RadioButton) findViewById(R.id.radio_main_p4);
+			radio_button[PLAYER_4].setText(PLAYER_NAMES[3]);
 			radiogroup_p24 = (RadioGroup) findViewById(R.id.radiogroup_main_p24);
 			radiogroup_p24.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 				@Override
@@ -86,19 +97,20 @@ public class MainActivity extends Activity implements OnClickListener {
 					RadioButton checkedRadioButton = (RadioButton) findViewById(checkedId);
 					String text = checkedRadioButton.getText().toString();
 
-					if (text.equals(radio_button[1].getText())) {
-						selected_p24 = 2;
+					if (text.equals(radio_button[PLAYER_2].getText())) {
+						selected_p24 = PLAYER_2;
 					}
-					else if (text.equals(radio_button[3].getText())) {
-						selected_p24 = 4;
+					else if (text.equals(radio_button[PLAYER_4].getText())) {
+						selected_p24 = PLAYER_4;
 					}
 				}
 			});
-			// Check settings to prevent them from sliding the seekbar to change their life.
+			// Check settings to enable/prevent them from sliding the seekbar to change their life.
 			if (Prefs.getLifeCounterTouchable(MainActivity.this)) {
-				PROGRESSBAR_LIFE[3].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { 
+				PROGRESSBAR_LIFE[PLAYER_4].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { 
 					public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-						updateViews(3);
+						life[PLAYER_4] = progress;
+						updateViews(PLAYER_4);
 					}
 
 					@Override
@@ -111,7 +123,7 @@ public class MainActivity extends Activity implements OnClickListener {
 				});
 			}
 			else {
-				PROGRESSBAR_LIFE[3].setOnTouchListener(new OnTouchListener(){
+				PROGRESSBAR_LIFE[PLAYER_4].setOnTouchListener(new OnTouchListener(){
 					@Override
 					public boolean onTouch(View v, MotionEvent event) {
 						return true;
@@ -119,17 +131,22 @@ public class MainActivity extends Activity implements OnClickListener {
 				});
 			}
 		case 3:
+			// Use the 3-player layout
 			if (NUM_PLAYERS == 3) {
 				setContentView(R.layout.activity_main3);
 			}
-			((TextView)findViewById(R.id.textview_main_p3_name)).setText(PLAYER_NAMES[2]);
+			
+			// Set Player 3's name
+			((TextView)findViewById(R.id.textview_main_p3_name)).setText(PLAYER_NAMES[PLAYER_3]);
 
-			TEXTVIEW_LIFE[2] = (TextView)findViewById(R.id.textview_main_p3_life);
-			PROGRESSBAR_LIFE[2] = (SeekBar) findViewById(R.id.progressbar_main_p3_life);
-			radio_button[0] = (RadioButton) findViewById(R.id.radio_main_p1);
-			radio_button[0].setText(PLAYER_NAMES[0]);
-			radio_button[2] = (RadioButton) findViewById(R.id.radio_main_p3);
-			radio_button[2].setText(PLAYER_NAMES[2]);
+			// Player 3's life text and progressbar
+			TEXTVIEW_LIFE[PLAYER_3] = (TextView)findViewById(R.id.textview_main_p3_life);
+			PROGRESSBAR_LIFE[PLAYER_3] = (SeekBar) findViewById(R.id.progressbar_main_p3_life);
+			// The radio group to select who the +-life buttons route to (player 1 or 3) 
+			radio_button[PLAYER_1] = (RadioButton) findViewById(R.id.radio_main_p1);
+			radio_button[PLAYER_1].setText(PLAYER_NAMES[PLAYER_1]);
+			radio_button[PLAYER_3] = (RadioButton) findViewById(R.id.radio_main_p3);
+			radio_button[PLAYER_3].setText(PLAYER_NAMES[PLAYER_3]);
 			radiogroup_p13 = (RadioGroup) findViewById(R.id.radiogroup_main_p13);
 			radiogroup_p13.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
 				@Override
@@ -138,19 +155,20 @@ public class MainActivity extends Activity implements OnClickListener {
 					RadioButton checkedRadioButton = (RadioButton) findViewById(checkedId);
 					String text = checkedRadioButton.getText().toString();
 
-					if (text.equals(radio_button[0].getText())) {
-						selected_p13 = 1;
+					if (text.equals(radio_button[PLAYER_1].getText())) {
+						selected_p13 = PLAYER_1;
 					}
-					else if (text.equals(radio_button[2].getText())) {
-						selected_p13 = 3;
+					else if (text.equals(radio_button[PLAYER_3].getText())) {
+						selected_p13 = PLAYER_3;
 					}
 				}
 			});
-			// Check settings to prevent them from sliding the seekbar to change their life.
+			// Check settings to enable/prevent them from sliding the seekbar to change their life.
 			if (Prefs.getLifeCounterTouchable(MainActivity.this)) {
-				PROGRESSBAR_LIFE[2].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { 
+				PROGRESSBAR_LIFE[PLAYER_3].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { 
 					public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-						updateViews(2);
+						life[PLAYER_3] = progress;
+						updateViews(PLAYER_3);
 					}
 
 					@Override
@@ -163,7 +181,7 @@ public class MainActivity extends Activity implements OnClickListener {
 				});
 			}
 			else {
-				PROGRESSBAR_LIFE[2].setOnTouchListener(new OnTouchListener(){
+				PROGRESSBAR_LIFE[PLAYER_3].setOnTouchListener(new OnTouchListener(){
 					@Override
 					public boolean onTouch(View v, MotionEvent event) {
 						return true;
@@ -171,16 +189,16 @@ public class MainActivity extends Activity implements OnClickListener {
 				});
 			}
 		case 2:
+			// Use the 2-player layout
 			if (NUM_PLAYERS == 2) {
 				setContentView(R.layout.activity_main);
 			}
-			// Set player names
-			((TextView)findViewById(R.id.textview_main_p1_name)).setText(PLAYER_NAMES[0]);
-			((TextView)findViewById(R.id.textview_main_p2_name)).setText(PLAYER_NAMES[1]);
+			
+			// Set Player 1 and 2's names
+			((TextView)findViewById(R.id.textview_main_p1_name)).setText(PLAYER_NAMES[PLAYER_1]);
+			((TextView)findViewById(R.id.textview_main_p2_name)).setText(PLAYER_NAMES[PLAYER_2]);
 
-			// Set buttons etc.
-			TEXTVIEW_LIFE[0] = (TextView)findViewById(R.id.textview_main_p1_life);
-			TEXTVIEW_LIFE[1] = (TextView)findViewById(R.id.textview_main_p2_life);
+			// Set up the +-life buttons
 			BUTTON_LIFE[0] = (Button) findViewById(R.id.button_main_p1_lifep1);
 			BUTTON_LIFE[1] = (Button) findViewById(R.id.button_main_p1_lifep5);
 			BUTTON_LIFE[2] = (Button) findViewById(R.id.button_main_p1_lifem1);
@@ -193,14 +211,17 @@ public class MainActivity extends Activity implements OnClickListener {
 				BUTTON_LIFE[i].setOnClickListener(this);
 			}
 
-			// Setup life meters
-			PROGRESSBAR_LIFE[0] = (SeekBar) findViewById(R.id.progressbar_main_p1_life);
-			PROGRESSBAR_LIFE[1] = (SeekBar) findViewById(R.id.progressbar_main_p2_life);
+			// Setup Player 1 and 2's life text and progressbars
+			TEXTVIEW_LIFE[PLAYER_1] = (TextView)findViewById(R.id.textview_main_p1_life);
+			TEXTVIEW_LIFE[PLAYER_2] = (TextView)findViewById(R.id.textview_main_p2_life);
+			PROGRESSBAR_LIFE[PLAYER_1] = (SeekBar) findViewById(R.id.progressbar_main_p1_life);
+			PROGRESSBAR_LIFE[PLAYER_2] = (SeekBar) findViewById(R.id.progressbar_main_p2_life);
+			
 			// Animate filling the life bars
 			for (int i = 0; i < PROGRESSBAR_LIFE.length; i++) {
 				final int player = i;
 				ValueAnimator anim = ValueAnimator.ofInt(0, PROGRESSBAR_LIFE[player].getMax());
-				anim.setDuration(4000);
+				anim.setDuration(3000);
 				anim.addUpdateListener(new AnimatorUpdateListener() {
 					@Override
 					public void onAnimationUpdate(ValueAnimator animation) {
@@ -211,12 +232,12 @@ public class MainActivity extends Activity implements OnClickListener {
 				anim.setStartDelay(300);
 				anim.start();
 			}
-			// Make life text visible after the animation
+			// Make life text visible after the animation, and set max progress to initial life
 			new AsyncTask<Void, Void, Void>() {
 				@Override
 				protected Void doInBackground(Void... params) {
 					try {
-						Thread.sleep(4300);
+						Thread.sleep(3300);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
@@ -229,14 +250,19 @@ public class MainActivity extends Activity implements OnClickListener {
 					for (int i = 0; i < NUM_PLAYERS; i++) {
 						TEXTVIEW_LIFE[i].setText(life[i] + " / " + STARTING_LIFE);
 						TEXTVIEW_LIFE[i].setTextColor(getResources().getColor(android.R.color.holo_blue_dark));
+						PROGRESSBAR_LIFE[i].setMax(STARTING_LIFE);
+						PROGRESSBAR_LIFE[i].setProgress(STARTING_LIFE);
 					}
 				}
 			}.execute();
-			// Check settings to prevent them from sliding the seekbar to change their life.
+			
+			// Check settings to enable/prevent them from sliding the seekbar to change their life.
 			if (Prefs.getLifeCounterTouchable(MainActivity.this)) {
-				PROGRESSBAR_LIFE[1].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { 
+				// Player 2
+				PROGRESSBAR_LIFE[PLAYER_2].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { 
 					public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-						updateViews(1);
+						life[PLAYER_2] = progress;
+						updateViews(PLAYER_2);
 					}
 
 					@Override
@@ -247,9 +273,11 @@ public class MainActivity extends Activity implements OnClickListener {
 					public void onStopTrackingTouch(SeekBar seekBar) {
 					}
 				});
-				PROGRESSBAR_LIFE[0].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { 
+				// Player 1
+				PROGRESSBAR_LIFE[PLAYER_1].setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { 
 					public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-						updateViews(0);
+						life[PLAYER_1] = progress;
+						updateViews(PLAYER_1);
 					}
 
 					@Override
@@ -263,13 +291,15 @@ public class MainActivity extends Activity implements OnClickListener {
 				});
 			}
 			else {
-				PROGRESSBAR_LIFE[1].setOnTouchListener(new OnTouchListener(){
+				// Player 2
+				PROGRESSBAR_LIFE[PLAYER_2].setOnTouchListener(new OnTouchListener(){
 					@Override
 					public boolean onTouch(View v, MotionEvent event) {
 						return true;
 					}
 				});
-				PROGRESSBAR_LIFE[0].setOnTouchListener(new OnTouchListener(){
+				// Player 1
+				PROGRESSBAR_LIFE[PLAYER_1].setOnTouchListener(new OnTouchListener(){
 					@Override
 					public boolean onTouch(View v, MotionEvent event) {
 						return true;
@@ -294,10 +324,9 @@ public class MainActivity extends Activity implements OnClickListener {
 		//TODO Activity lifecycle stuff
 	}
 
-	/** Update life */
+	/** Update life for a specific player */
 	void updateViews(int p) {
 		final int player = p;
-		life[player] = (int)(PROGRESSBAR_LIFE[player].getProgress()/1000.0*STARTING_LIFE);
 		TEXTVIEW_LIFE[player].setText(life[player] + " / " + STARTING_LIFE);
 
 		// Color the text based on life
@@ -318,46 +347,18 @@ public class MainActivity extends Activity implements OnClickListener {
 			color = android.R.color.holo_red_dark;
 		}
 		TEXTVIEW_LIFE[player].setTextColor(getResources().getColor(color));
-	}
-
-	/** Update life and progress bars */
-	void updateViews(int p, int increment) {
-		final int player = p;
-		life[player] += increment;
-		TEXTVIEW_LIFE[player].setText(life[player] + " / " + STARTING_LIFE);
-
-		//TODO make smooth with successive touches
-		ValueAnimator anim = ValueAnimator.ofInt((int)((life[player] + -1*increment)*1000.0/STARTING_LIFE), (int)(life[player]*1000.0/STARTING_LIFE));
-		anim.setDuration(2000);
-		anim.addUpdateListener(new AnimatorUpdateListener() {
-			@Override
-			public void onAnimationUpdate(ValueAnimator animation) {
-				int animProgress = (Integer) animation.getAnimatedValue();
-				PROGRESSBAR_LIFE[player].setProgress(animProgress);
+		
+		for (int i = 0; i < life.length; i++) {
+			if (life[i] <= 0) {
+				// If someone has 0 or less life, make a gg dialog and mark them as dead (avoid multiple dialogs for same person).
+				if (alive[i]) {
+					alive[i] = false;
+					gameOver(i);
+					break;
+				}
 			}
-		});
-		anim.start();
-
-		// Color the text based on life
-		int color = 0;
-		if (life[player] > STARTING_LIFE) {
-			color = android.R.color.holo_purple;
 		}
-		else if (life[player] > STARTING_LIFE*3.0/4.0) {
-			color = android.R.color.holo_blue_dark;
-		}
-		else if (life[player] > STARTING_LIFE*2.0/4.0) {
-			color = android.R.color.holo_green_dark;
-		}
-		else if (life[player] > STARTING_LIFE/4.0) {
-			color = android.R.color.holo_orange_dark;
-		}
-		else if (life[player] <= STARTING_LIFE/4.0) {
-			color = android.R.color.holo_red_dark;
-		}
-		TEXTVIEW_LIFE[player].setTextColor(getResources().getColor(color));
 	}
-
 
 	/** Handle when a button is clicked, call appropriate action */
 	public void onClick(View v) 
@@ -365,43 +366,54 @@ public class MainActivity extends Activity implements OnClickListener {
 		switch (v.getId()) 
 		{
 		case R.id.button_main_p1_lifep1:
-			updateViews( selected_p13 == 1?0:2, 1);
+			// Adjust the selected player's life, then update the textview
+			life[selected_p13] += 1;
+			PROGRESSBAR_LIFE[selected_p13].setProgress(life[selected_p13]);
+			updateViews(selected_p13);
 			break;
 		case R.id.button_main_p1_lifep5:
-			updateViews(selected_p13 == 1?0:2, 5);
+			life[selected_p13] += 5;
+			PROGRESSBAR_LIFE[selected_p13].setProgress(life[selected_p13]);
+			updateViews(selected_p13);
 			break;
 		case R.id.button_main_p1_lifem1:
-			updateViews(selected_p13 == 1?0:2, -1);
+			life[selected_p13] -= 1;
+			PROGRESSBAR_LIFE[selected_p13].setProgress(life[selected_p13]);
+			updateViews(selected_p13);
 			break;
 		case R.id.button_main_p1_lifem5:
-			updateViews(selected_p13 == 1?0:2, -5);
+			life[selected_p13] -= 5;
+			PROGRESSBAR_LIFE[selected_p13].setProgress(life[selected_p13]);
+			updateViews(selected_p13);
 			break;
 		case R.id.button_main_p2_lifep1:
-			updateViews(selected_p24 == 2?1:3, 1);
+			life[selected_p24] += 1;
+			PROGRESSBAR_LIFE[selected_p24].setProgress(life[selected_p24]);
+			updateViews(selected_p24);
 			break;
 		case R.id.button_main_p2_lifep5:
-			updateViews(selected_p24 == 2?1:3, 5);
+			life[selected_p24] += 5;
+			PROGRESSBAR_LIFE[selected_p24].setProgress(life[selected_p24]);
+			updateViews(selected_p24);
 			break;
 		case R.id.button_main_p2_lifem1:
-			updateViews(selected_p24 == 2?1:3, -1);
+			life[selected_p24] -= 1;
+			PROGRESSBAR_LIFE[selected_p24].setProgress(life[selected_p24]);
+			updateViews(selected_p24);
 			break;
 		case R.id.button_main_p2_lifem5:
-			updateViews(selected_p24 == 2?1:3, -5);
+			life[selected_p24] -= 5;
+			PROGRESSBAR_LIFE[selected_p24].setProgress(life[selected_p24]);
+			updateViews(selected_p24);
 			break;
-		}
-
-		for (int i = 0; i < life.length; i++) {
-			if (life[i] < 0) {
-				gameOver(i + 1);
-				break;
-			}
 		}
 	}
 
-	/** Someone has won */
+	/** Someone has lost */
 	private void gameOver(int player) {
+		// Make an AlertDialog with a loss message
 		AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-		alertDialog.setTitle("Player " + player + " has lost.");
+		alertDialog.setTitle(PLAYER_NAMES[player] + " has lost.");
 		alertDialog.setMessage("GG.");
 		alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Continue", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int which) {
@@ -414,11 +426,12 @@ public class MainActivity extends Activity implements OnClickListener {
 		alertDialog.show();
 	}
 
-	/** Use KitKat Immersive Mode if possible */
+	/** Use KitKat Immersive Mode if possible otherwise just fullscreen */
 	@TargetApi(android.os.Build.VERSION_CODES.KITKAT)
 	@Override
 	public void onWindowFocusChanged(boolean hasFocus) {
 		super.onWindowFocusChanged(hasFocus);
+		// Immersive mode for kitkat
 		if (Prefs.getImmersiveMode(MainActivity.this) && android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT && hasFocus) {
 			getWindow().getDecorView().setSystemUiVisibility(
 					View.SYSTEM_UI_FLAG_LAYOUT_STABLE
