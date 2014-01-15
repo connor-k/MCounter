32e4f56ca7d5115d8e8eb336f72956b7d4c931df
connor-k <quikcon@cox.net>
2014-01-14 17:42:11 -0800

    Added initial layout and code for choosing player mana color

--- src/com/connork/mtgcounter/SetupActivity.java
+++ src/com/connork/mtgcounter/SetupActivity.java
@@ -29,8 +29,11 @@ import android.widget.TextView;
 public class SetupActivity extends Activity implements OnClickListener {
 	private Menu optionsMenu;
 	private int num_players, starting_life;
+	private final int MANA_WHITE = 0, MANA_BLUE = 1, MANA_BLACK = 2, MANA_RED = 3, MANA_GREEN = 4;
+	private int[] mana_color = {MANA_WHITE, MANA_WHITE, MANA_WHITE, MANA_WHITE}; // The mana color code for each player
 	private String player_names[] = new String[4];
-	private Button button_players[] = new Button[4];
+	private Button button_players[] = new Button[4], button_mana[] = new Button[4];
+	private Button button_continue; //TODO
 	private RadioGroup radio_group;
 	private RadioButton radio_button[] = new RadioButton[3];
 
@@ -47,7 +50,7 @@ public class SetupActivity extends Activity implements OnClickListener {
 		bitmapDrawable.setTileModeXY(Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
 		actionBar.setBackgroundDrawable(bitmapDrawable);
 
-		// Set up click listeners for all the buttons
+		// Set up click listeners for all the player name buttons
 		button_players[0] = (Button) findViewById(R.id.button_setup_p1);
 		button_players[1] = (Button) findViewById(R.id.button_setup_p2);
 		button_players[2] = (Button) findViewById(R.id.button_setup_p3);
@@ -56,6 +59,16 @@ public class SetupActivity extends Activity implements OnClickListener {
 			button_players[i].setOnClickListener(this);
 		}
 
+		// Set up click listeners for all the player mana colors
+		button_mana[0] = (Button) findViewById(R.id.button_setup_p1_mana);
+		button_mana[1] = (Button) findViewById(R.id.button_setup_p2_mana);
+		button_mana[2] = (Button) findViewById(R.id.button_setup_p3_mana);
+		button_mana[3] = (Button) findViewById(R.id.button_setup_p4_mana);
+		for (int i = 0; i < 4; i++) {
+			button_mana[i].setOnClickListener(this);
+		}
+
+		// Set up the radio group to select how many people are playing
 		radio_button[0] = (RadioButton) findViewById(R.id.radio_setup_2p);
 		radio_button[1] = (RadioButton) findViewById(R.id.radio_setup_3p);
 		radio_button[2] = (RadioButton) findViewById(R.id.radio_setup_4p);
@@ -114,14 +127,13 @@ public class SetupActivity extends Activity implements OnClickListener {
 			} 
 		}); 
 
-		//TODO Add settings, keep screen on
-
-		// Restore last state if possible
+		// Restore last state if possible, otherwise get the player names etc from storage
 		if (savedInstanceState != null)
 		{
 			num_players = savedInstanceState.getInt("setup_num_players", 2);
 			player_names = savedInstanceState.getStringArray("setup_player_names");
 			starting_life = savedInstanceState.getInt("setup_starting_life", 20);
+			mana_color = savedInstanceState.getIntArray("setup_mana_color");
 		}
 		else {
 			String names = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("KEY_PLAYER_NAMES", "Player 1, Player 2, Player 3, Player 4");
@@ -129,37 +141,51 @@ public class SetupActivity extends Activity implements OnClickListener {
 			num_players = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getInt("KEY_NUM_PLAYERS", 2);
 			starting_life = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getInt("KEY_STARTING_LIFE", 20);
 		}
+		// Select the appropriate radio button for how many people are playing
 		radio_button[num_players - 2].toggle();
+		// Set button text to player names
 		for (int i = 0; i < 4; i++) {
 			button_players[i].setText(player_names[i]);
+			setManaColor(i, mana_color[i]);
 		}
+		// Set the initial life progress bar to previous state
 		seekBar.setProgress(starting_life);
 		seekBarText.setText("Initial Life: " + starting_life);
 	} 
 
 	/** Handle when a button is clicked, call appropriate action */
-	public void onClick(View v) 
-	{
+	public void onClick(View v) {
 		switch (v.getId()) 
 		{
 		case R.id.button_setup_p1:
-			editPlayerName(1);
+			editPlayerName(MainActivity.PLAYER_1);
 			break;
 		case R.id.button_setup_p2:
-			editPlayerName(2); 
+			editPlayerName(MainActivity.PLAYER_2); 
 			break;
 		case R.id.button_setup_p3:
-			editPlayerName(3);
+			editPlayerName(MainActivity.PLAYER_3);
 			break;
 		case R.id.button_setup_p4:
-			editPlayerName(4); 
+			editPlayerName(MainActivity.PLAYER_4); 
+			break;
+		case R.id.button_setup_p1_mana:
+			editPlayerMana(MainActivity.PLAYER_1);
+			break;
+		case R.id.button_setup_p2_mana:
+			editPlayerMana(MainActivity.PLAYER_2); 
+			break;
+		case R.id.button_setup_p3_mana:
+			editPlayerMana(MainActivity.PLAYER_3);
+			break;
+		case R.id.button_setup_p4_mana:
+			editPlayerMana(MainActivity.PLAYER_4); 
 			break;
 		}
 	}
 
 	/** Set a player's name */
-	private void editPlayerName(int p)
-	{
+	private void editPlayerName(int p) {
 		final int player = p;
 		AlertDialog alertDialog = new AlertDialog.Builder(SetupActivity.this).create();
 		alertDialog.setTitle("Edit Player Name");
@@ -167,7 +193,7 @@ public class SetupActivity extends Activity implements OnClickListener {
 		// Set an EditText view to get name   
 		final EditText input = new EditText(this); 
 		input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
-		input.setText(player_names[player - 1]);
+		input.setText(player_names[player]);
 		int position = input.length();
 		Editable etext = input.getText();
 		Selection.setSelection(etext, position);
@@ -175,8 +201,60 @@ public class SetupActivity extends Activity implements OnClickListener {
 		alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Done", new DialogInterface.OnClickListener() {
 			public void onClick(DialogInterface dialog, int which) {
 				String name = input.getText().toString();
-				player_names[player - 1] = name;
-				button_players[player - 1].setText(name);
+				player_names[player] = name;
+				button_players[player].setText(name);
+			} });
+		alertDialog.show();
+	}
+
+	/** Change a player's mana icon */
+	private void setManaColor(int player, int color) {
+		switch (color) {
+		default:
+			button_mana[player].setBackgroundDrawable(getResources().getDrawable(R.drawable.mana_white));
+			break;
+		case MANA_BLUE:
+			button_mana[player].setBackgroundDrawable(getResources().getDrawable(R.drawable.mana_blue));
+			break;
+		case MANA_BLACK:
+			button_mana[player].setBackgroundDrawable(getResources().getDrawable(R.drawable.mana_black));
+			break;
+		case MANA_RED:
+			button_mana[player].setBackgroundDrawable(getResources().getDrawable(R.drawable.mana_red));
+			break;
+		case MANA_GREEN:
+			button_mana[player].setBackgroundDrawable(getResources().getDrawable(R.drawable.mana_green));
+			break;
+		}
+	}
+
+	/** Dialog to set a player's mana color */
+	private void editPlayerMana(int p) {
+		final int player = p;
+		final AlertDialog alertDialog = new AlertDialog.Builder(SetupActivity.this).create();
+		alertDialog.setTitle("Choose " + player_names[player] + "\'s Mana Color");
+		alertDialog.setView(getLayoutInflater().inflate(R.layout.alertdialog_setup_mana, null));
+		alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Done", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				RadioGroup radioGroup = (RadioGroup)alertDialog.findViewById(R.id.radiogroup_setup_mana);
+
+				switch (radioGroup.getCheckedRadioButtonId()) {
+				case R.id.radio_setup_mana_white:
+					setManaColor(player, MANA_WHITE);
+					break;
+				case R.id.radio_setup_mana_blue:
+					setManaColor(player, MANA_BLUE);
+					break;
+				case R.id.radio_setup_mana_black:
+					setManaColor(player, MANA_BLACK);
+					break;
+				case R.id.radio_setup_mana_red:
+					setManaColor(player, MANA_RED);
+					break;
+				case R.id.radio_setup_mana_green:
+					setManaColor(player, MANA_GREEN);
+					break;
+				}
 			} });
 		alertDialog.show();
 	}
@@ -197,6 +275,7 @@ public class SetupActivity extends Activity implements OnClickListener {
 			intent.putExtra(MainActivity.KEY_NUM_PLAYERS, num_players);
 			intent.putExtra(MainActivity.KEY_PLAYER_NAMES, player_names);
 			intent.putExtra(MainActivity.KEY_STARTING_LIFE, starting_life);
+			//TODO pass mana color
 			startActivity(intent);
 			return true;
 		case R.id.menu_main_settings:
@@ -223,27 +302,24 @@ public class SetupActivity extends Activity implements OnClickListener {
 
 
 	@Override
-	protected void onPause()
-	{
-
+	protected void onPause() {
 		super.onPause();
 	}
 
 	@Override
-	protected void onDestroy()
-	{
+	protected void onDestroy() {
 		// Save the player names, etc.
-		String names = player_names[0] + ',' + player_names[1] + ',' + player_names[2] + ',' + player_names[3];
+		String names = player_names[MainActivity.PLAYER_1] + ',' + player_names[MainActivity.PLAYER_2] + ',' + player_names[MainActivity.PLAYER_3] + ',' + player_names[MainActivity.PLAYER_4];
 		PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit().putString("KEY_PLAYER_NAMES", names).commit();
 		PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit().putInt("KEY_NUM_PLAYERS", num_players).commit();
 		PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit().putInt("KEY_STARTING_LIFE", starting_life).commit();
+		//TODO save chosen mana color
+
 		super.onDestroy();
 	}
 
 	@Override
-	protected void onResume() 
-	{
-
+	protected void onResume() {
 		super.onResume();
 	}
 
@@ -254,5 +330,6 @@ public class SetupActivity extends Activity implements OnClickListener {
 		outState.putInt("setup_num_players", num_players);
 		outState.putStringArray("setup_player_names", player_names);
 		outState.putInt("setup_starting_life", starting_life);
+		outState.putIntArray("setup_mana_color", mana_color);
 	}
 }
