bd837588a36e4883bccf9aef6f51ec666e939bcc
Connor Kerns <connor-k@users.noreply.github.com>
2015-05-17 14:23:32 -0700

    Add a constants class

---
+++ src/com/connork/mcounter/Constants.java
@@ -0,0 +1,21 @@
+package com.connork.mcounter;
+
+/**
+ * Constants used by classes in this package.
+ */
+class Constants {
+    // Player codes
+	public static final int MAX_PLAYERS = 4;
+    public static final int PLAYER_1 = 0;
+    public static final int PLAYER_2 = 1;
+    public static final int PLAYER_3 = 2;
+    public static final int PLAYER_4 = 3;
+    
+    // Mana colors
+    public static final int MANA_COLORLESS = 0;
+    public static final int MANA_WHITE = 1;
+    public static final int MANA_BLUE = 2;
+    public static final int MANA_BLACK = 3;
+    public static final int MANA_RED = 4;
+    public static final int MANA_GREEN = 5;
+}
